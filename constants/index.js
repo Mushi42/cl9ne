243d8b2dfcaae989be4e5b0966c92f4985f9ebac
Aqib Ijaz <aqib.ijaz@gigalabs.co>
2022-05-28 00:58:24 +0500

    strip live key added

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,9 @@
-export const BASE_URL = 'http://localhost:11000'
-export const STRIPE_PUB_KEY = 'pk_test_3bYHNArgCLURNCNZViaJ2Cyi00s1L6F6WQ';
+export const BASE_URL = 'http://localhost:11000';
+
+// ---------------------- Strip ---------------------------------
+// dev
+// export const STRIPE_PUB_KEY = 'pk_test_3bYHNArgCLURNCNZViaJ2Cyi00s1L6F6WQ';
+
+// live
+export const STRIPE_PUB_KEY =
+  'pk_live_51Kn6xMC1JBwLApYY2lRPvhU7ZDdxZbU55s2Kgfs9c1M98NHq7DdR6xNam84mp6ADGgus1nbgJVMfQa6JlSofrxMm00bvihBCLO';
